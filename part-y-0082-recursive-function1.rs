0ac2909bcbb4d1b641cef5c77d58ab6b5f22b4f3
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-02-10 09:14:24 +0300

    Create part-y-0082-recursive-function1.rs

---
+++ part-y-0082-recursive-function1.rs
@@ -0,0 +1,25 @@
+// before recursive function!
+
+fn main() {
+
+    println!("{}", factorial(5));
+
+    println!("{}", factorial(15));
+
+    println!("{}", factorial(0));
+}
+
+fn factorial(x: u128) -> u128 {
+
+    let mut r: u128 = 1;
+
+    for i in 1..= x {
+
+        r *= i;
+    }
+    r
+}
+
+// 120
+// 1307674368000
+// 1
